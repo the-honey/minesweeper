3b28e04f5b3143a085eb31ba93003da48663b89c
Mézes István <1valaki1asd@gmail.com>
2024-05-26 16:49:50 +0200

    Remove pwa plugin

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,20 +1,8 @@
 <script lang="ts">
 	import '../app.css';
-	import { pwaInfo } from 'virtual:pwa-info';
 
 	let { children } = $props();
-
-	let webManifest = $state('');
-	$effect(() => {
-		if (pwaInfo) {
-			webManifest = pwaInfo.webManifest.linkTag;
-		}
-	});
 </script>
-
-<svelte:head>
-	{@html webManifest}
-</svelte:head>
 
 <main class="h-dvh box-content content-center">
 	{@render children()}
--- vite.config.ts
+++ vite.config.ts
@@ -1,64 +1,64 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 import { defineConfig } from 'vite';
 import Icons from 'unplugin-icons/vite';
+// import { SvelteKitPWA } from '@vite-pwa/sveltekit';
 
 export default defineConfig({
 	plugins: [
 		sveltekit(),
-		SvelteKitPWA({
-			srcDir: './src',
-			mode: 'development',
-			strategies: 'generateSW',
-			scope: '/',
-			base: '/',
-			// selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
-			manifest: {
-				short_name: 'SvelteKit PWA',
-				name: 'SvelteKit PWA',
-				start_url: '/',
-				scope: '/',
-				display: 'standalone',
-				theme_color: '#ffffff',
-				background_color: '#ffffff',
-				icons: [
-					{
-						src: '/favicon.png',
-						sizes: '128x128',
-						type: 'image/png'
-					}
-					// {
-					// 	src: '/pwa-512x512.png',
-					// 	sizes: '512x512',
-					// 	type: 'image/png'
-					// },
-					// {
-					// 	src: '/pwa-512x512.png',
-					// 	sizes: '512x512',
-					// 	type: 'image/png',
-					// 	purpose: 'any maskable'
-					// }
-				]
-			},
-			injectManifest: {
-				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
-			},
-			workbox: {
-				globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
-			},
-			devOptions: {
-				enabled: true,
-				// suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
-				type: 'module',
-				navigateFallback: '/'
-			},
-			// if you have shared info in svelte config file put in a separate module and use it also here
-			kit: {
-				includeVersionFile: true
-			}
-		}),
 		Icons({
 			compiler: 'svelte'
 		})
+		// SvelteKitPWA({
+		// 	srcDir: './src',
+		// 	mode: 'development',
+		// 	strategies: 'generateSW',
+		// 	scope: '/',
+		// 	base: '/',
+		// 	// selfDestroying: process.env.SELF_DESTROYING_SW === 'true',
+		// 	manifest: {
+		// 		short_name: 'SvelteKit PWA',
+		// 		name: 'SvelteKit PWA',
+		// 		start_url: '/',
+		// 		scope: '/',
+		// 		display: 'standalone',
+		// 		theme_color: '#ffffff',
+		// 		background_color: '#ffffff',
+		// 		icons: [
+		// 			{
+		// 				src: '/favicon.png',
+		// 				sizes: '128x128',
+		// 				type: 'image/png'
+		// 			}
+		// 			// {
+		// 			// 	src: '/pwa-512x512.png',
+		// 			// 	sizes: '512x512',
+		// 			// 	type: 'image/png'
+		// 			// },
+		// 			// {
+		// 			// 	src: '/pwa-512x512.png',
+		// 			// 	sizes: '512x512',
+		// 			// 	type: 'image/png',
+		// 			// 	purpose: 'any maskable'
+		// 			// }
+		// 		]
+		// 	},
+		// 	injectManifest: {
+		// 		globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+		// 	},
+		// 	workbox: {
+		// 		globPatterns: ['client/**/*.{js,css,ico,png,svg,webp,woff,woff2}']
+		// 	},
+		// 	devOptions: {
+		// 		enabled: true,
+		// 		// suppressWarnings: process.env.SUPPRESS_WARNING === 'true',
+		// 		type: 'module',
+		// 		navigateFallback: '/'
+		// 	},
+		// 	// if you have shared info in svelte config file put in a separate module and use it also here
+		// 	kit: {
+		// 		includeVersionFile: true
+		// 	}
+		// }),
 	]
 });
