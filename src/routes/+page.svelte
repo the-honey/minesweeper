ee57fec8bda152e910b884759d55014d721bb3d1
Mézes István <1valaki1asd@gmail.com>
2024-07-15 17:17:43 +0200

    feat: Shorter long press

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -92,7 +92,7 @@
 				<div class="flex justify-center">
 					{#each row as cell, cellIndex}
 						<button
-							use:press={{ timeframe: 300, triggerBeforeFinished: true }}
+							use:press={{ timeframe: 200, triggerBeforeFinished: true }}
 							on:press={() => minesweeper.onFieldPress(cellIndex, rowIndex)}
 							on:click={() => minesweeper.onFieldClick(cellIndex, rowIndex)}
 							class="btn lg:btn-md btn-sm m-0.5 btn-square text-lg lg:text-2xl"
