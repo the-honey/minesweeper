ae5eb238c8a929c6626e500c5cc72de9ae896e2e
Mézes István <1valaki1asd@gmail.com>
2024-07-15 17:18:00 +0200

    fix: Remove text selection on long press

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -4,6 +4,6 @@
 	let { children } = $props();
 </script>
 
-<main class="h-dvh box-content content-center">
+<main class="h-dvh box-content select-none content-center">
 	{@render children()}
 </main>
