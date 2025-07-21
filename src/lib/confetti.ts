1cd6eb879c0635279dc73876f075f1dd135689b6
the-honey <1valaki1asd@gmail.com>
2025-07-21 18:52:50 +0200

    fix: confetti

--- src/lib/confetti.ts
+++ src/lib/confetti.ts
@@ -1,43 +1,43 @@
-import { confetti } from '@tsparticles/confetti';
+import { confetti, type ConfettiFirstParam } from '@tsparticles/confetti';
 
 const count = 200,
-	defaults = {
-		origin: { y: 0.7 }
-	};
+  defaults: ConfettiFirstParam = {
+    origin: { y: 0.7 },
+  };
 
-function fire(particleRatio: number, opts: object) {
-	confetti(
-		Object.assign({}, defaults, opts, {
-			particleCount: Math.floor(count * particleRatio)
-		})
-	);
+async function fire(particleRatio: number, opts: object) {
+  await confetti(
+    Object.assign({}, defaults, opts, {
+      particleCount: Math.floor(count * particleRatio)
+    })
+  );
 }
 
-export default function fireConfetti() {
-	fire(0.25, {
-		spread: 26,
-		startVelocity: 55
-	});
+export default async function fireConfetti() {
+  await fire(0.25, {
+    spread: 26,
+    startVelocity: 55
+  });
 
-	fire(0.2, {
-		spread: 60
-	});
+  await fire(0.2, {
+    spread: 60
+  });
 
-	fire(0.35, {
-		spread: 100,
-		decay: 0.91,
-		scalar: 0.8
-	});
+  await fire(0.35, {
+    spread: 100,
+    decay: 0.91,
+    scalar: 0.8
+  });
 
-	fire(0.1, {
-		spread: 120,
-		startVelocity: 25,
-		decay: 0.92,
-		scalar: 1.2
-	});
+  await fire(0.1, {
+    spread: 120,
+    startVelocity: 25,
+    decay: 0.92,
+    scalar: 1.2
+  });
 
-	fire(0.1, {
-		spread: 120,
-		startVelocity: 45
-	});
+  await fire(0.1, {
+    spread: 120,
+    startVelocity: 45
+  });
 }
